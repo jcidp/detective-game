79ef826fb0325a089ec080307089159417537032
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-05-05 18:36:17 -0600

    Setup frontend with React

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl my-8 text-center'>Detective Game</h1>
+      </header>
+      <main>
+        <p className='text-2xl text-center my-4'>Find the selected characters in the picture as fast as you can!</p>
+        <p className='text-2xl text-center'>When you find one, click on it and select the character.</p>
+        <div className="games">
+          
+        </div>
+      </main>
+      <footer>
+
+      </footer>
     </>
   )
 }
