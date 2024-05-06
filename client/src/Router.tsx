9b2ab2726a6f2dd1bc9324d08e70a3bd6556f957
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-05-05 19:30:21 -0600

    Create basic page layout

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,12 +1,17 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import Home from "./pages/Home";
 
 const Router = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <App />,
-    },
+      children: [
+        {index: true, element: <Home />},
+        {path: "/game/:name", element: <div>Game!</div>}
+      ]
+    }
   ]);
 
 
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <>
+      <p className='text-2xl text-center my-4'>Find the selected characters in the picture as fast as you can!</p>
+      <p className='text-2xl text-center'>When you find one, click on it and select the character.</p>
+      <div className="games">
+      </div>
+    </>
+  );
+};
+
+export default Home;
