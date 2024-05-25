ba9fd17cfc51483e05218ec18d5b973182fc95ab
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-05-25 17:58:06 -0600

    Setup Rails & React client to work together

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api, :defaults => {:format => :json} do
+    namespace :v1 do
+      resources :puzzles, only: [:index]
+      resources :characters, only: [:index]
+    end
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
