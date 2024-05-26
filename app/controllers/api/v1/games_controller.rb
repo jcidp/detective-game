274b29f39106bbb8eec63b69182ade608c577518
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-05-25 23:50:42 -0600

    Create a Game in the controller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::GamesController < ApplicationController
   def create
+    puzzle = Puzzle.find_by(name: params[:puzzle_name])
+    @game = puzzle.game.build
+    if @game.save
+      render json: @game.id, status: :created
+    else
+      render json: @game.errors, status: :unprocessable_entity
+    end
   end
 
   def update
--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -2,4 +2,5 @@ class Puzzle < ApplicationRecord
   validates :name, :image_url, presence: true
 
   has_many :characters
+  has_many :games
 end
