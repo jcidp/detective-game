156647bb8e7d3508806400e4dba6440f74a5c25d
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-06-02 17:48:39 -0600

    Load games from API in Home page

--- app/controllers/api/v1/puzzles_controller.rb
+++ app/controllers/api/v1/puzzles_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::PuzzlesController < ApplicationController
+  def index
+    @puzzles = Puzzle.all
+    render json: @puzzles
+  end
+  
   def show
     @puzzle = Puzzle.find_by(name: params[:id])
     @characters = @puzzle.characters
--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,7 +1,13 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import useFetchData from "@/hooks/useFetchData";
+import { PuzzleI } from "@/types";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [puzzles, error, loading] = useFetchData<PuzzleI[]>({url: "/api/v1/puzzles"});
+
+  if (error) return <p>Something went wrong. Please try again later.</p>
+
   return (
     <>
       <div className="intro text-2xl text-center my-4">
@@ -9,17 +15,24 @@ const Home = () => {
         <p>When you find one, click on it and select the character.</p>
       </div>
       <div className="games mt-8 grid grid-cols-3">
-        <Link to="puzzles/waldo">
-          <Card>
-            <CardHeader>
-              <CardTitle>Where's Waldo</CardTitle>
-              <CardDescription>The classic character finder game</CardDescription>
-            </CardHeader>
-            <CardContent>
-              SomeImage
-            </CardContent>
-          </Card>
-        </Link>
+        {
+          loading ? <p>Loading...</p> :
+          puzzles?.map(puzzle => {
+            return (
+              <Link to={`puzzles/${puzzle.name}`} key={puzzle.name} >
+                <Card>
+                  <CardHeader>
+                    <CardTitle>{puzzle.name.toLowerCase().replace(/\b\w/g, s => s.toUpperCase())}</CardTitle>
+                    <CardDescription>Description placeholder text</CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    Image Placeholder
+                  </CardContent>
+                </Card>
+              </Link>
+            );
+          })
+        }
       </div>
     </>
   );
