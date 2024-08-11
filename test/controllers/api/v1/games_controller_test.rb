4378e60350013c700cf24e5546ced9088c5c69a6
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-08-11 17:31:34 -0600

    Add Controllers Unit Tests

--- test/controllers/api/v1/games_controller_test.rb
+++ test/controllers/api/v1/games_controller_test.rb
@@ -1,7 +1,46 @@
 require "test_helper"
 
 class Api::V1::GamesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create & show game" do
+    puzzle = puzzles(:one)
+
+    assert_difference("Game.count") do
+      get api_v1_game_url(puzzle)
+    end
+
+    assert_equal Game.order(created_at: :desc).first.id.to_json, @response.body
+  end
+
+  test "should update game and not return highscores or index" do
+    game = games(:four)
+    character = characters(:one)
+  
+    put "/api/v1/games/#{game.id}", params: { id: game.id, name: character.name, x: 1.6, y: 1.8 }, as: :json
+
+    game.reload
+    expected_response = {game: game, highscores: nil, index: nil}.to_json
+    assert_equal JSON.parse(expected_response), JSON.parse(@response.body)
+  end
+
+  test "should update game and return highscores and index" do
+    game = games(:five)
+    character = characters(:four)
+  
+    put "/api/v1/games/#{game.id}", params: { id: game.id, name: character.name, x: 1.6, y: 1.8 }, as: :json
+
+    game.reload
+    expected_response = {game: game, highscores: [games(:six), game], index: 1}.to_json
+    assert_equal JSON.parse(expected_response), JSON.parse(@response.body)
+  end
+
+  test "should update game username" do
+    game = games(:five)
+
+    put "/api/v1/games/#{game.id}/username", params: { id: game.id, username: "test" }
+
+    game.reload
+    assert_equal "test", game.username
+    expected_response = {status: "Success"}.to_json
+    assert_equal JSON.parse(expected_response), JSON.parse(@response.body)
+  end
 end
--- test/controllers/api/v1/puzzles_controller_test.rb
+++ test/controllers/api/v1/puzzles_controller_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class Api::V1::PuzzlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get api_v1_puzzles_url
+    assert_equal [puzzles(:one), puzzles(:two), puzzles(:three)].to_json, @response.body
+  end
+
+  test "should show puzzle" do
+    puzzle = puzzles(:one)
+    get api_v1_puzzle_url(puzzle)
+    puzzle_json = {puzzle: puzzle, characters: puzzle.characters}.to_json
+    assert_equal puzzle_json, @response.body
+  end
 end
